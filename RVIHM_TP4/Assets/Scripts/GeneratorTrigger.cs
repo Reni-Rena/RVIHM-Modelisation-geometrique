ac23d2fb3cc02734530cd148f48628c6a4a3d6ed
Reni-Rena <miyukibuf5@gmail.com>
2025-06-15 22:45:25 +0200

    door and robot

--- RVIHM_TP4/Assets/Scripts/GeneratorTrigger.cs
+++ RVIHM_TP4/Assets/Scripts/GeneratorTrigger.cs
@@ -3,13 +3,15 @@ using Unity.AI.Navigation;
 
 public class GeneratorTrigger : MonoBehaviour
 {
-    public GameObject obstacle;
+    public GameObject obstacle1;
+    public GameObject obstacle2;
     public NavMeshSurface surface;
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            obstacle.SetActive(false);
+            obstacle1.SetActive(false);
+            obstacle2.SetActive(false);
             surface.BuildNavMesh();
         }
     }
